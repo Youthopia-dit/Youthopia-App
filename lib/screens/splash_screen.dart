9371f848321ad481cdd4adc5612b715cca4d1b8a
Garimagrawal5113 <garimagrawal5113.com>
2023-09-27 16:03:58 +0530

    splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -4,6 +4,35 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('Assets/image 89.png'),
+            fit: BoxFit.fill,
+                      ),
+        ),
+        child: Column(
+          children: <Widget>[
+          Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Image.asset('Assets/naac.png'),
+                Image.asset('Assets/DIT LOGO WHITE.png'),
+                Image.asset('Assets/25_years_logo.png'),
+    ]
+    ),
+          Row(
+             mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+             children: <Widget>[
+
+             Image.asset('Assets/Group 6623.png'),
+          ]
+          )
+    ]
+  ),
+      ),
+    );
   }
 }
