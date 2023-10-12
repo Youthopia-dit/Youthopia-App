5415d12da0f67867d9eac62ae6aecb2c0dec0b7a
Garimagrawal5113 <garimagrawal5113.com>
2023-10-12 16:31:02 +0530

    empty events commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:youthopia/screens/college_screen.dart';
 import 'package:youthopia/screens/splash_screen.dart';
+import 'package:youthopia/screens/test_screen.dart';
+import 'package:youthopia/widgets/empty_events.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: CollegeScreen(),
+      home: TestScreens(),
     );
   }
 }
