b4433b87ff92a2501c18e417009090db84c40d29
Shivang-i <shivangirajput2212@gmail.com>
2023-10-13 17:22:29 +0530

    profile second commit

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -5,4 +5,6 @@ class CustomColors{
   static const Grey1 = Color(0xFF666666);
   static const Black40 = Color(0x99000000);
   static const black = Color(0xFF000000);
+  static const grey = Color(0xFFD9D9D9);
+
 }
