eaace4a543fc66610b5ac9420ceb65bab1cfc23f
Pratik S <pratiksingh.sskr@gmail.com>
2023-10-13 13:27:13 +0530

    signupprogress

--- lib/Screens/signup_screen.dart
+++ lib/Screens/signup_screen.dart
@@ -14,46 +14,87 @@ class SignUpPage extends StatelessWidget {
         body: BackgroundScaffold(
             child: Center(
                 child: InkWellRectangle(
-          child: TextInputWidget(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SignupHeading(),
+              SizedBox(height: 10),
+              TextFormField(
+                decoration: InputDecoration(
+                    hintText: 'Name',
+                    fillColor: Colors.white,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(28))),
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                decoration: InputDecoration(
+                    hintText: 'Email',
+                    fillColor: Colors.white,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10))),
+              ),
+            ],
+          ),
         ))),
       ),
     );
   }
 }
 
-class TextInputWidget extends StatelessWidget {
+class SignupHeading extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        _buildTextField('Name'),
-        SizedBox(height: 10),
-        _buildTextField('Email'),
-        SizedBox(height: 10),
-        _buildTextField('Phone'),
-        SizedBox(height: 10),
-        _buildTextField('Gender'),
-      ],
-    );
-  }
-
-  Widget _buildTextField(String label) {
     return Container(
-      width: 209.0,
-      height: 37.0,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(5.0),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10.0),
-        child: TextField(
-          decoration: InputDecoration(
-            hintText: label,
-            border: InputBorder.none,
+      padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: MediaQuery.of(context).size.height / 13,
+            color: Colors.transparent, // Changed color to transparent
+            child: Center(
+              child: Text(
+                'Sign Up', // Added "Sign Up" text
+                style: TextStyle(
+                  fontSize: 20.0, // Changed font size
+                  color: Colors.white, // Changed text color to white
+                ),
+              ),
+            ),
           ),
-        ),
+          Container(
+            height: MediaQuery.of(context).size.height / 13,
+            color: Colors.transparent, // Changed color to transparent
+            child: Column(
+              children: [
+                Expanded(
+                  child: Container(
+                    color: Colors.transparent, // Color of the first container
+                    child: Image.asset(
+                        'Assets/Group 6481.png'), // Add your image asset
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    color: Colors.blue, // Color of the second container
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
