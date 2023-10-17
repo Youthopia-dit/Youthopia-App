d14530639800558d60582891d0bfa5c312e48564
TheDarkLord100 <miracleyt404@gmail.com>
2023-10-17 15:19:13 +0530

    api initiated

--- lib/data/backend.dart
+++ lib/data/backend.dart
@@ -1,17 +1,15 @@
+import 'package:http/http.dart' as http;
+
 class Api {
-  final String baseUrl = 'https://youthopiabackend.azurewebsites.net/';
+  final String baseUrl = 'youthopiabackend.azurewebsites.net';
 
-  Future<void> login() async {
+  Future<void> login() async {}
 
-  }
-
-  Future<void> signup() async {
-
-  }
-
+  Future<void> signup() async {}
 
   Future<void> getEventDetails() async {
-
+    final Url = Uri.https(baseUrl, '/event/geteventdetails');
+    final response = await http.get(Url);
+    print(response.body);
   }
-
 }
